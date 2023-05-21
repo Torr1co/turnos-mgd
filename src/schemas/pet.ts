41a31b2f438a555107439626df45cdf5b8a51a42
Torr1co <fabriziotorrico27@gmail.com>
2023-05-21 19:32:09 -0300

    pull

--- src/schemas/pet.ts
+++ src/schemas/pet.ts
@@ -1,23 +1,27 @@
 import { z } from "zod";
 
 export const PetSchema = z.object({
-  name: z.string().min(1,"Minimo 1 caracter").max(20,"Maximo 20 caracteres"),
+  name: z.string().min(1, "Minimo 1 caracter").max(20, "Maximo 20 caracteres"),
   birth: z.date({
-    required_error : "Requerido",
-    invalid_type_error : "Requerido"
+    required_error: "Requerido",
+    invalid_type_error: "Requerido",
   }),
   gender: z.string(),
   color: z.string(),
-  weight: z.number({
-    required_error : "Requerido",
-    invalid_type_error : "Requerido"
-  }).max(150,"No puede pesar mas de 150"),
-  height: z.number({
-    required_error : "Requerido",
-    invalid_type_error : "Requerido"
-  }).max(200,"No puede medir mas de 200"),
+  weight: z
+    .number({
+      required_error: "Requerido",
+      invalid_type_error: "Requerido",
+    })
+    .max(150, "No puede pesar mas de 150"),
+  height: z
+    .number({
+      required_error: "Requerido",
+      invalid_type_error: "Requerido",
+    })
+    .max(200, "No puede medir mas de 200"),
   img: z.optional(z.string()),
-  race: z.string().min(1,"Minimo 1 caracter"),
+  race: z.string().min(1, "Minimo 1 caracter"),
   castrated: z.optional(z.boolean()).default(false),
   letsCross: z.optional(z.boolean()).default(false),
   observations: z.optional(z.string()).default(""),
