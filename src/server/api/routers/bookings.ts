e840d5ba6e5bce60c020f723479a798b986aad48
Ramiro Gutierrez <ramagp00@gmail.com>
2023-05-21 18:18:31 -0300

    booking create

--- src/server/api/routers/bookings.ts
+++ src/server/api/routers/bookings.ts
@@ -15,6 +15,7 @@ export const bookingsRouter = createTRPCRouter({
     .mutation(async ({ input, ctx }) => {
       const { dog, user, ...booking } = input;
 
+      //TODO: Move this to another file
       // Check if the date is in the past or a Sunday
       if (dayjs(booking.date).isBefore(dayjs(), "day"))
         throw new Error("No puedes reservar en el pasado!");
@@ -40,25 +41,51 @@ export const bookingsRouter = createTRPCRouter({
           "No puedes crear una cita para un usuario que no existe!"
         );
 
+      //TODO: Move this to another file
       //Check if the dog is in conditions to get the vaccine
-      const dogData = await ctx.prisma.pet.findUnique({
-        where: {
-          id: dog,
-        },
-      });
-      // if (booking.type === "VACCINEB") {
-      //   //Check if the dog is older than 4 months
-      //   if (dayjs(dogData?.birth).isAfter(dayjs().subtract(4, "month"))) {
-      //     throw new Error("El perro es menor de 4 meses!");
-      //   }
-      //   //Check if the dog has the last vaccine at least 1 year ago
-      //   if (dayjs(dogData?.healthBook).isAfter(dayjs().subtract(1, "year"))) {
+      if (booking.type === "VACCINE") {
+        const dogData = await ctx.prisma.pet.findUnique({
+          where: {
+            id: dog,
+          },
+        });
+        if (booking.vaccine === "B") {
+          //TODO: Change this validation to a healtBook validation
 
-      //   }
-      // }
-      if (booking.type === "VACCINEA") {
-        if (dayjs(dogData?.birth).isAfter(dayjs().subtract(4, "month"))) {
-          throw new Error("El perro es menor de 4 meses!");
+          //Check if the dog has a rabies vaccine in the last year
+          const lastVaccineB = await ctx.prisma.booking.findFirst({
+            where: {
+              dog: {
+                id: dog,
+              },
+              vaccine: {
+                equals: "B",
+              },
+              date: {
+                gte: dayjs().subtract(1, "year").toDate(),
+              },
+            },
+          });
+          if (lastVaccineB) {
+            throw new Error(
+              "No se puede aplicar una antirrabica a un perro que ya tiene una en el ultimo año!"
+            );
+          }
+          //Check if the dog is older than 4 months
+          if (dayjs(dogData?.birth).isBefore(dayjs().subtract(4, "month"))) {
+            throw new Error(
+              "No se puede aplicar una antirrabica a un perro menor de 4 meses!"
+            );
+          }
+          //TODO: Add a new booking in a year
+        }
+        if (booking.vaccine === "A") {
+          //Check if the dog is older than 4 months
+          if (dayjs(dogData?.birth).isBefore(dayjs().subtract(4, "month"))) {
+            //TODO: Add a new booking in 21 days
+          } else {
+            //TODO: Add a new booking in a year
+          }
         }
       }
 
