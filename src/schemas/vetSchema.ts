9a7665f4a167bdd22b62375fe6ce30e594bbea5a
Torr1co <fabriziotorrico27@gmail.com>
2023-06-14 14:54:11 -0300

    email trim

--- src/schemas/vetSchema.ts
+++ src/schemas/vetSchema.ts
@@ -5,7 +5,7 @@ export const VetCreationSchema = z.object({
   name: z.string().min(3),
   dni: z.string().min(7),
   lastname: z.string().min(3),
-  email: z.string().email(),
+  email: z.string().trim().email(),
   password: z.string().min(8),
   role: z.nativeEnum(UserRoles),
 });
